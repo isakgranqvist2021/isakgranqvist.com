8a02b727898b962a3a4d9d50c77bb19be01b870d
isakgranqvist2021 <isakwebdev@gmail.com>
2021-09-06 06:58:59 +0200

    stuff

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -15,19 +15,31 @@ function About(props) {
 	return (
 		<div id='About' className='bg-dark'>
 			<div className='container'>
-				<h3>What is Lorem Ipsum?</h3>
-				<p>
-					Lorem Ipsum is simply dummy text of the printing and
-					typesetting industry. Lorem Ipsum has been the industry's
-					standard dummy text ever since the 1500s, when an unknown
-					printer took a galley of type and scrambled it to make a
-					type specimen book. It has survived not only five centuries,
-					but also the leap into electronic typesetting, remaining
-					essentially unchanged. It was popularised in the 1960s with
-					the release of Letraset sheets containing Lorem Ipsum
-					passages, and more recently with desktop publishing software
-					like Aldus PageMaker including versions of Lorem Ipsum.
-				</p>
+				<div className='flex-item'>
+					<h3>What is Lorem Ipsum?</h3>
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and
+						typesetting industry. Lorem Ipsum has been the
+						industry's standard dummy text ever since the 1500s,
+						when an unknown printer took a galley of type and
+						scrambled it to make a type specimen book. It has
+						survived not only five centuries, but also the leap into
+						electronic typesetting, remaining essentially unchanged.
+						It was popularised in the 1960s with the release of
+						Letraset sheets containing Lorem Ipsum passages, and
+						more recently with desktop publishing software like
+						Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+				</div>
+				<div className='flex-item'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						width='24'
+						height='24'
+						viewBox='0 0 24 24'>
+						<path d='M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.033 16.01c.564-1.789 1.632-3.932 1.821-4.474.273-.787-.211-1.136-1.74.209l-.34-.64c1.744-1.897 5.335-2.326 4.113.613-.763 1.835-1.309 3.074-1.621 4.03-.455 1.393.694.828 1.819-.211.153.25.203.331.356.619-2.498 2.378-5.271 2.588-4.408-.146zm4.742-8.169c-.532.453-1.32.443-1.761-.022-.441-.465-.367-1.208.164-1.661.532-.453 1.32-.442 1.761.022.439.466.367 1.209-.164 1.661z' />
+					</svg>
+				</div>
 			</div>
 		</div>
 	);
